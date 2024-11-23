8643ae8b2ee729806d3509062e42ed2053238978
UmgsServices <149515826+UmgsServices@users.noreply.github.com>
2024-11-23 01:29:30 +0100

    base url

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL='http://localhost:5000'
+//const BASE_URL='http://localhost:5000'
+const BASE_URL='https://car-back-s4as.onrender.com'
 export default axios.create({
     baseURL:BASE_URL,
     headers:{'Content-Type':'application/json'},
